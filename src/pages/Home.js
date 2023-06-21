3a57e9f57219166f3fe288ee44979afeea7eb4e2
Avinash S <96516762+avinashshivappa@users.noreply.github.com>
2023-06-21 13:33:06 +0530

    Update Home.js

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,7 @@
 import React from "react";
-import "../styles/Home.css";
 import { Link } from "react-router-dom";
 import BannerImage from "../assets/pizza.jpeg"
+import "../styles/Home.css";
 
 
 
